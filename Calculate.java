e9fe67ab86ba4f56d3970e889ed6b91da6fee337
Иванов Олег Викторович <oivanov@bbank.ru>
2017-06-16 16:30:55 +0800

    modify Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,26 @@
 public class Calculate {
 	public static void main (String[] arg) {
 		System.out.println("Calculate...");
-	
+		double dFirst = Double.valueOf(arg[0]);
+		double dSecond = Double.valueOf(arg[1]);
+		int iFirst = (int)dFirst;
+		int iSecond = (int)dSecond;
+		//int iFirst = Integer.valueOf(arg[0]);
+		//int iSecond = Integer.valueOf(arg[1]);
+		int summ = iFirst + iSecond;
+		int subtraction = iFirst - iSecond;
+		int multiplication = iFirst * iSecond;
+		int division = iFirst / iSecond;
+		int pow = 1;
+		while (iSecond > 0) {
+			pow = pow * iFirst;
+			iSecond = iSecond - 1;
+		}
+		
+		System.out.println("Sum  " + summ);
+		System.out.println("Sub  " + subtraction);
+		System.out.println("mul  " + multiplication);
+		System.out.println("div  " + division);
+		System.out.println("pow  " + pow);
 	}
 }
