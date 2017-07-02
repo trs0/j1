4dcdb871cd01c8d5c64216c794e990acee213769
trs <hellgi@ngs.ru>
2017-07-03 00:11:06 +0800

    mod Calculate

--- Calculate.java
+++ Calculate.java
@@ -7,20 +7,53 @@ public class Calculate {
 		int iSecond = (int)dSecond;
 		//int iFirst = Integer.valueOf(arg[0]);
 		//int iSecond = Integer.valueOf(arg[1]);
-		int summ = iFirst + iSecond;
-		int subtraction = iFirst - iSecond;
-		int multiplication = iFirst * iSecond;
-		int division = iFirst / iSecond;
-		int pow = 1;
-		while (iSecond > 0) {
-			pow = pow * iFirst;
-			iSecond = iSecond - 1;
-		}
+				
+			
+		int summ = Summ(iFirst, iSecond);
+		int subtraction = Sub(iFirst, iSecond);
+		double multiplication = Mult(dFirst, dSecond);
+		double division = Div(dFirst, dSecond);
+		int pow = Pow(iFirst, iSecond);
 		
 		System.out.println("Sum  " + summ);
 		System.out.println("Sub  " + subtraction);
 		System.out.println("mul  " + multiplication);
 		System.out.println("div  " + division);
 		System.out.println("pow  " + pow);
-	}
+		
+		}
+		
+		
+	static int Summ (int first, int second) {
+		int summ = first + second;
+		return summ;
+		}
+	
+	static int Sub (int first, int second)
+		{
+		int subtraction = first - second;
+		return subtraction;
+		}
+		
+	static double Mult (double first, double second)
+		{
+		double multiplication = first * second;
+		return multiplication;
+		}
+		
+	static double Div (double first, double second)
+		{
+		double division = first / second;
+		return division;
+		}
+		
+	static int Pow (int first, int second)
+		{
+		int pow = 1;
+		while (second > 0) {
+			pow = pow * first;
+			second = second - 1;
+			}
+			return pow;
+		}
 }
